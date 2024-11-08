3bba33ce7e2bb722b7f9f5686d0791f8a498c831
AtroMusic <sadeqatro916@gmail.com>
2024-11-08 11:41:54 -0800

    Update google.py

--- plugins/google.py
+++ plugins/google.py
@@ -6,7 +6,7 @@ from SafoneAPI import SafoneAPI
 from YukkiMusic import app
 
 
-@app.on_message(filters.command(["google", "gle"]))
+@app.on_message(filters.command(["google", "gle","گوگل"]))
 async def google(bot, message):
     if len(message.command) < 2 and not message.reply_to_message:
         await message.reply_text("Example:\n\n`/google lord ram`")
@@ -31,7 +31,7 @@ async def google(bot, message):
         logging.exception(e)
 
 
-@app.on_message(filters.command(["app", "apps"]))
+@app.on_message(filters.command(["app", "apps","برنامه"]))
 async def app(bot, message):
     if len(message.command) < 2 and not message.reply_to_message:
         await message.reply_text("Example:\n\n`/app Free Fire`")
