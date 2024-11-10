4050f60e005ad330e9c75e66f44af4274923145a
AtroMusic <sadeqatro916@gmail.com>
2024-11-10 13:44:24 -0800

    Update websource.py

--- plugins/websource.py
+++ plugins/websource.py
@@ -50,7 +50,7 @@ def web_download(client, message):
         message.reply_document(document="website.txt", caption=f"Source code of {url}")
 
 
-__MODULE__ = "Wᴇʙᴅʟ"
+# __MODULE__ = "Wᴇʙᴅʟ"
 __HELP__ = """
 **ᴄᴏᴍᴍᴀɴᴅ:**
 
