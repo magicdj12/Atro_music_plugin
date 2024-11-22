a7771d093f8e86ce27c00a0a5145e97c824569e4
AtroMusic <sadeqatro916@gmail.com>
2024-11-22 23:09:21 +0430

    Update chatgpt.py

--- plugins/chatgpt.py
+++ plugins/chatgpt.py
@@ -3,6 +3,10 @@ from pyrogram import filters
 from pyrogram.enums import ChatAction
 from TheApi import api
 from YukkiMusic import app
+import requests
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from YukkiMusic import app
+
 
 
 @app.on_message(filters.command(["chatgpt", "ai", "ask"]) & ~BANNED_USERS)
