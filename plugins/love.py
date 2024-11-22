6b7197948071a3128aa116b994e54e2090a87fed
AtroMusic <sadeqatro916@gmail.com>
2024-11-22 23:04:20 +0430

    Update love.py

--- plugins/love.py
+++ plugins/love.py
@@ -1,7 +1,6 @@
 import random
 
 from pyrogram import filters
-
 from YukkiMusic import app
 
 
@@ -9,32 +8,30 @@ def get_random_message(love_percentage):
     if love_percentage <= 30:
         return random.choice(
             [
-                "عشق در هوا موج می‌زند اما به کمی جرقه نیاز دارد.",
-                "شروع خوبی است اما جای پیشرفت دارد.",
-                "این فقط آغاز چیزی زیباست.",
-
+                "Love is in the air but needs a little spark.",
+                "A good start but there's room to grow.",
+                "It's just the beginning of something beautiful.",
             ]
         )
     elif love_percentage <= 70:
         return random.choice(
             [
-                "یک ارتباط قوی وجود دارد. به پرورشش ادامه دهید.",
-                "شانس خوبی دارید. روی آن کار کنید.",
-                "عشق در حال شکوفایی است، ادامه دهید."
-
+                "A strong connection is there. Keep nurturing it.",
+                "You've got a good chance. Work on it.",
+                "Love is blossoming, keep going.",
             ]
         )
     else:
         return random.choice(
             [
-            "وای! این یک پیوند آسمانی است!",
-            "جفتی کامل! این رابطه را گرامی بدارید.",
-            "سرنوشت شما با هم بودن است. تبریک می‌گویم!"
+                "Wow! It's a match made in heaven!",
+                "Perfect match! Cherish this bond.",
+                "Destined to be together. Congratulations!",
             ]
         )
 
 
-@app.on_message(filters.command(["love","عشق","علاقه","دوست"],prefixes=['','/']))
+@app.on_message(filters.command("love", prefixes="/"))
 def love_command(client, message):
     command, *args = message.text.split(" ")
     if len(args) >= 2:
@@ -46,14 +43,13 @@ def love_command(client, message):
 
         response = f"{name1}💕 + {name2}💕 = {love_percentage}%\n\n{love_message}"
     else:
-        response = "لطفا دو نام را انتخاب کنید /عشق دستور."
+        response = "Please enter two names after /love command."
     app.send_message(message.chat.id, response)
 
 
-__MODULE__ = "عشق"
+__MODULE__ = "Lᴏᴠᴇ"
 __HELP__ = """
-**ماشین حساب عشق:**
+**ʟᴏᴠᴇ ᴄᴀʟᴄᴜʟᴀᴛᴏʀ:**
 
-• `/love [نام1] [نام2]`: درصد عشق بین دو نفر را محاسبه می‌کند.
-• `/عشق [نام1] [نام2]`: درصد عشق بین دو نفر را محاسبه می‌کند.
+• `/love [name1] [name2]`: Cᴀʟᴄᴜʟᴀᴛᴇs ᴛʜᴇ ᴘᴇʀᴄᴇɴᴛᴀɢᴇ ᴏғ ʟᴏᴠᴇ ʙᴇᴛᴡᴇᴇɴ ᴛᴡᴏ ᴘᴇᴏᴘʟᴇ.
 """
