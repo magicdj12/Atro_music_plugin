1e5986d504c6965fcefd936d29cb36c3f77220b0
AtroMusic <sadeqatro916@gmail.com>
2024-11-22 23:01:42 +0430

    Update chatgpt.py

--- plugins/chatgpt.py
+++ plugins/chatgpt.py
@@ -1,16 +1,15 @@
+from config import BANNED_USERS
 from pyrogram import filters
 from pyrogram.enums import ChatAction
 from TheApi import api
-
 from YukkiMusic import app
-from config import BANNED_USERS
 
 
-@app.on_message(filters.command(["جی پی تی", "هوش مشصنوعی", "سوال",'bard','بارد','chatgpt'],prefixes=['','/']) & ~BANNED_USERS)
+@app.on_message(filters.command(["chatgpt", "ai", "ask"]) & ~BANNED_USERS)
 async def chatgpt_chat(bot, message):
     if len(message.command) < 2 and not message.reply_to_message:
         await message.reply_text(
-            "نمونه:\n\n`/سوال چگونه از حساب خود حفاظت کنیم?`"
+            "Example:\n\n`/ai write simple website code using html css, js?`"
         )
         return
 
@@ -19,30 +18,13 @@ async def chatgpt_chat(bot, message):
     else:
         user_input = " ".join(message.command[1:])
 
-    results = api.chatgpt(user_input)
     await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
+    results = await api.chatgpt(user_input)
     await message.reply_text(results)
 
 
-
-__MODULE__ = "هوش مصنوعی◉"
+__MODULE__ = "CʜᴀᴛGᴘᴛ"
 __HELP__ = """
-◉هوش مصنوعی
- 
-با استفاده از دستور زیر خبرها و...هرچیزی که بخواین دریافت کنید 🔻
-𝄞 گوگل
-/google [آب و هو]
-
-با استفاده از دستور زیر هر نوع برنامه ای که میخواین دانلود کنید🔻
-𝄞برنامه
-/app [فیلتیر شکن]
-
-
-با استفاده از دستور زیر سوال خودرا چت جی پی تی بپرسید 🔻
-𝄞بارد 
-/bard [ ترامپ چه کسیست]
-با استفاده از دستور زیر سوال خودرا از هوش مصنوعی بپرسید🔻
-𝄞هوشم
-/ai
-
-"""
+/advice - ɢᴇᴛ ʀᴀɴᴅᴏᴍ ᴀᴅᴠɪᴄᴇ ʙʏ ʙᴏᴛ
+/ai [ǫᴜᴇʀʏ] - ᴀsᴋ ʏᴏᴜʀ ǫᴜᴇsᴛɪᴏɴ ᴡɪᴛʜ ᴄʜᴀᴛɢᴘᴛ's ᴀɪ
+/gemini [ǫᴜᴇʀʏ] - ᴀsᴋ ʏᴏᴜʀ ǫᴜᴇsᴛɪᴏɴ ᴡɪᴛʜ ɢᴏᴏɢʟᴇ's ɢᴇᴍɪɴɪ ᴀɪ"""
