2ab45406c4a91fffc6879dd626345094116d4d41
AtroMusic <sadeqatro916@gmail.com>
2024-12-17 20:48:36 -0800

    Update bots.py

--- plugins/bots.py
+++ plugins/bots.py
@@ -1,45 +1,49 @@
 import asyncio
 from datetime import datetime
-from convertdate import islamic, jalali
+import jdatetime  # برای تاریخ شمسی
 from pyrogram import filters
 from YukkiMusic import app
 
-# نام ماه‌های شمسی و قمری
-PERSIAN_MONTHS = [
+# نام ماه‌های شمسی و میلادی به فارسی
+PERSIAN_SOLAR_MONTHS = [
     "فروردین", "اردیبهشت", "خرداد", "تیر", "مرداد", "شهریور",
     "مهر", "آبان", "آذر", "دی", "بهمن", "اسفند"
 ]
-ARABIC_MONTHS = [
-    "محرم", "صفر", "ربیع الاول", "ربیع الثانی", "جمادی الاولى",
-    "جمادی الثانیه", "رجب", "شعبان", "رمضان", "شوال", "ذی القعده", "ذی الحجه"
+
+PERSIAN_GREGORIAN_MONTHS = [
+    "ژانویه", "فوریه", "مارس", "آوریل", "مه", "ژوئن",
+    "ژوئیه", "اوت", "سپتامبر", "اکتبر", "نوامبر", "دسامبر"
 ]
 
 # توابع تبدیل تاریخ
 def gregorian_to_jalali(date):
-    y, m, d = jalali.from_gregorian(date.year, date.month, date.day)
-    return f"{d} {PERSIAN_MONTHS[m-1]} {y}"
+    jalali_date = jdatetime.date.fromgregorian(day=date.day, month=date.month, year=date.year)
+    day = jalali_date.day
+    month = PERSIAN_SOLAR_MONTHS[jalali_date.month - 1]
+    year = jalali_date.year
+    return f"{day} {month} {year}"
 
-def gregorian_to_islamic(date):
-    y, m, d = islamic.from_gregorian(date.year, date.month, date.day)
-    return f"{d} {ARABIC_MONTHS[m-1]} {y}"
+def gregorian_to_persian(date):
+    day = date.day
+    month = PERSIAN_GREGORIAN_MONTHS[date.month - 1]
+    year = date.year
+    return f"{day} {month} {year}"
 
 # تابع اصلی برای نمایش ساعت و تاریخ
-@app.on_message(filters.command("time") & filters.group)
+@app.on_message(filters.command("datetime") & filters.group)
 async def show_datetime(client, message):
     try:
         # اطلاعات تاریخ و ساعت
         now = datetime.now()
         jalali_date = gregorian_to_jalali(now)
-        islamic_date = gregorian_to_islamic(now)
-        gregorian_date = now.strftime("%A - %Y %d %B")
+        gregorian_date = gregorian_to_persian(now)
         current_time = now.strftime("%I:%M %p")  # نمایش ساعت به فرمت 12 ساعته
 
         # متن خروجی
         text = f"""ساعت و تاریخ:
 
 • ساعت: {current_time}
-• تاریخ امروز: {jalali_date}
-• تاریخ قمری: {islamic_date}
+• تاریخ امروز (شمسی): {jalali_date}
 • تاریخ میلادی: {gregorian_date}
 """
 
@@ -48,11 +52,3 @@ async def show_datetime(client, message):
     except Exception as e:
         await asyncio.sleep(1)
         print(f"Error: {e}")
-
-# اطلاعات ماژول
-MODULE = "ساعت و تاریخ"
-HELP = """
-با این دستور می‌توانید ساعت و تاریخ کنونی را در سه فرمت (شمسی، قمری، میلادی) مشاهده کنید:
-دستور:
-/datetime
-"""
