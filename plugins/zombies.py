4762b46de1924cd1fea194507d8cc7ab6d955fca
AtroMusic <sadeqatro916@gmail.com>
2024-12-19 04:08:21 -0800

    Update zombies.py

--- plugins/zombies.py
+++ plugins/zombies.py
@@ -1,69 +1,126 @@
 from YukkiMusic import app
 from pyrogram import Client, filters
-from pyrogram.types import Message
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 
-# ذخیره‌سازی کانال‌های اجباری و وضعیت جوین
+# لیست کانال‌های اجباری و وضعیت جوین
 forced_channels = []
-join_required = False
-ADMIN_ID = [1924774929]
-
-@app.on_message(filters.command("add_join") & filters.user(ADMIN_ID))
-async def add_join_channel(client: Client, message: Message):
-    await message.reply("لینک یا آیدی عددی کانال مورد نظر را ارسال کنید:")
-    reply = await client.listen(message.chat.id)  # دریافت پاسخ کاربر
-    channel_id = reply.text.strip()
-
-    if channel_id not in forced_channels:
-        forced_channels.append(channel_id)
-        await message.reply(f"کانال {channel_id} به لیست جوین اجباری اضافه شد.")
-    else:
-        await message.reply("این کانال قبلاً در لیست وجود دارد!")
+join_required = True
 
 
-@app.on_message(filters.command("list_join") & filters.user(ADMIN_ID))
-async def list_join_channels(client: Client, message: Message):
-    if forced_channels:
-        channels = "\n".join(forced_channels)
-        await message.reply(f"لیست کانال‌های جوین اجباری:\n{channels}")
-    else:
-        await message.reply("هیچ کانالی در لیست جوین اجباری وجود ندارد.")
+@app.on_message(filters.text & filters.private)
+async def handle_commands(client: Client, message: Message):
+    text = message.text.strip().lower()
+
+    # دستورات اضافه کردن کانال
+    if text in ["اضافه کردن جوین", "add join"]:
+        await message.reply("لینک یا آیدی عددی کانال را ارسال کنید:\nSend the link or numeric ID of the channel:")
+        reply = await client.listen(message.chat.id)
+        channel_id = reply.text.strip()
+
+        if channel_id not in forced_channels:
+            forced_channels.append(channel_id)
+            await message.reply(
+                f"کانال {channel_id} به لیست جوین اجباری اضافه شد.\nChannel {channel_id} has been added to the forced join list."
+            )
+        else:
+            await message.reply(
+                "این کانال قبلاً در لیست وجود دارد.\nThis channel is already in the list."
+            )
+
+    # دستورات نمایش لیست کانال‌ها
+    elif text in ["نمایش لیست جوین", "show join list"]:
+        if forced_channels:
+            buttons = [
+                [
+                    InlineKeyboardButton(
+                        text="عضویت در کانال / Join Channel",
+                        url=f"https://t.me/{channel.replace('@', '')}",
+                    )
+                ]
+                for channel in forced_channels
+            ]
+            buttons.append([InlineKeyboardButton("عضو شدم / Joined", callback_data="check_join")])
+            await message.reply(
+                "لیست کانال‌های جوین اجباری:\nList of forced join channels:",
+                reply_markup=InlineKeyboardMarkup(buttons),
+            )
+        else:
+            await message.reply("هیچ کانالی در لیست وجود ندارد.\nNo channels in the list.")
+
+    # دستورات حذف کانال
+    elif text in ["حذف جوین", "remove join"]:
+        await message.reply("لینک یا آیدی کانالی که می‌خواهید حذف کنید را ارسال کنید:\nSend the link or ID of the channel to remove:")
+        reply = await client.listen(message.chat.id)
+        channel_id = reply.text.strip()
+
+        if channel_id in forced_channels:
+            forced_channels.remove(channel_id)
+            await message.reply(
+                f"کانال {channel_id} از لیست حذف شد.\nChannel {channel_id} has been removed from the list."
+            )
+        else:
+            await message.reply(
+                "این کانال در لیست وجود ندارد.\nThis channel is not in the list."
+            )
+
+    # دستورات روشن کردن جوین
+    elif text in ["جوین روشن", "enable join"]:
+        global join_required
+        join_required = True
+        await message.reply("جوین اجباری فعال شد.\nForced join has been enabled.")
+
+    # دستورات خاموش کردن جوین
+    elif text in ["جوین خاموش", "disable join"]:
+        join_required = False
+        await message.reply("جوین اجباری غیرفعال شد.\nForced join has been disabled.")
 
 
-@app.on_message(filters.command("remove_join") & filters.user(ADMIN_ID))
-async def remove_join_channel(client: Client, message: Message):
-    await message.reply("لینک یا آیدی عددی کانالی که می‌خواهید حذف کنید را ارسال کنید:")
-    reply = await client.listen(message.chat.id)  # دریافت پاسخ کاربر
-    channel_id = reply.text.strip()
+@app.on_callback_query(filters.regex("check_join"))
+async def check_user_join(client: Client, callback_query):
+    if join_required and forced_channels:
+        user_id = callback_query.from_user.id
+        not_joined = []
 
-    if channel_id in forced_channels:
-        forced_channels.remove(channel_id)
-        await message.reply(f"کانال {channel_id} از لیست جوین اجباری حذف شد.")
-    else:
-        await message.reply("این کانال در لیست جوین اجباری یافت نشد!")
+        for channel in forced_channels:
+            try:
+                await client.get_chat_member(channel, user_id)
+            except:
+                not_joined.append(channel)
 
-
-@app.on_message(filters.command("join_on") & filters.user(ADMIN_ID))
-async def enable_join(client: Client, message: Message):
-    global join_required
-    join_required = True
-    await message.reply("جوین اجباری فعال شد. کاربران باید عضو کانال‌های مشخص‌شده شوند.")
-
-
-@app.on_message(filters.command("join_off") & filters.user(ADMIN_ID))
-async def disable_join(client: Client, message: Message):
-    global join_required
-    join_required = False
-    await message.reply("جوین اجباری غیرفعال شد. نیازی به عضویت کاربران نیست.")
+        if not not_joined:
+            await callback_query.message.reply(
+                "😎 عالی! حالا می‌تونی از ربات استفاده کنی!\nAwesome! You can now use the bot!"
+            )
+        else:
+            await callback_query.message.reply(
+                "🤨 هنوز عضو همه کانال‌ها نشدی!\nYou haven't joined all the channels yet!"
+            )
 
 
 @app.on_message(filters.private, group=-1)
-async def check_user_join(client: Client, message: Message):
+async def enforce_join(client: Client, message: Message):
     if join_required and forced_channels:
+        not_joined = []
+
         for channel in forced_channels:
             try:
                 await client.get_chat_member(channel, message.from_user.id)
             except:
-                await message.reply(
-                    f"برای استفاده از ربات، ابتدا عضو کانال زیر شوید:\n{channel}"
-                )
-                return
+                not_joined.append(channel)
+
+        if not_joined:
+            buttons = [
+                [
+                    InlineKeyboardButton(
+                        text="عضویت در کانال / Join Channel",
+                        url=f"https://t.me/{channel.replace('@', '')}",
+                    )
+                ]
+                for channel in not_joined
+            ]
+            buttons.append([InlineKeyboardButton("عضو شدم / Joined", callback_data="check_join")])
+            await message.reply(
+                "برای استفاده از ربات، ابتدا در کانال‌های زیر عضو شوید:\nTo use the bot, join the following channels first:",
+                reply_markup=InlineKeyboardMarkup(buttons),
+            )
+            return
