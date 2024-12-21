3165d74c862880292780c1842bb8a0ac733e744f
AtroMusic <sadeqatro916@gmail.com>
2024-12-21 03:32:24 -0800

    Update love.py

--- plugins/love.py
+++ plugins/love.py
@@ -1,28 +1,28 @@
-from pyrogram import filters
-from pyrogram.types import Message
-from YukkiMusic import app
+# from pyrogram import filters
+# from pyrogram.types import Message
+# from YukkiMusic import app
 
-def bold(x):
-    return f"**{x}:** "
-
-
-def bold_ul(x):
-    return f"**--{x}:**-- "
+# def bold(x):
+#     return f"**{x}:** "
 
 
-def mono(x):
-    return f"`{x}`\n"
+# def bold_ul(x):
+#     return f"**--{x}:**-- "
 
-@app.on_message(filters.command(["عشق",'love'],prefixes=["", "/"]))
-async def chat_info_func(_, message: Message):
-    chat = message.chat.id
-    user_input = message.text.split(' ')
-    message.reply(user_input)
-    if len(user_input) == 3:
-        message.reply(chat,user_input)
 
-# __MODULE__ = "عشق"
-__HELP__ = """
-• عشق نام کاربر نام کاربری دوم
-• عشق سلیم فواد
-"""
+# def mono(x):
+#     return f"`{x}`\n"
+
+# @app.on_message(filters.command(["عشق",'love'],prefixes=["", "/"]))
+# async def chat_info_func(_, message: Message):
+#     chat = message.chat.id
+#     user_input = message.text.split(' ')
+#     message.reply(user_input)
+#     if len(user_input) == 3:
+#         message.reply(chat,user_input)
+
+# # __MODULE__ = "عشق"
+# __HELP__ = """
+# • عشق نام کاربر نام کاربری دوم
+# • عشق سلیم فواد
+# """
