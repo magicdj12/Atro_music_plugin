67aaec6a20fa1fa656059668061b7e3bbca570d1
AtroMusic <sadeqatro916@gmail.com>
2024-12-22 23:51:44 -0800

    Update group.py

--- plugins/group.py
+++ plugins/group.py
@@ -1,29 +1,30 @@
 from pyrogram import enums, filters
 from YukkiMusic import app
+
 from utils.permissions import adminsOnly
 
 
-@app.on_message(filters.command(["removephoto","حذف بروف"]))
+@app.on_message(filters.command("removephoto"))
 @adminsOnly("can_change_info")
 async def deletechatphoto(_, message):
 
     chat_id = message.chat.id
     user_id = message.from_user.id
-    msg = await message.reply_text("در حال پردازش....")
+    msg = await message.reply_text("**ᴘʀᴏᴄᴇssɪɴɢ....**")
     admin_check = await app.get_chat_member(chat_id, user_id)
     if message.chat.type == enums.ChatType.PRIVATE:
-        await msg.edit("این دستور فقط در گروه‌ها قابل استفاده است!")  # This command works only in groups
+        await msg.edit("**ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ ᴡᴏʀᴋ ᴏɴ ɢʀᴏᴜᴘs !**")
     try:
         if admin_check.privileges.can_change_info:
             await app.delete_chat_photo(chat_id)
             await msg.edit(
-                "عکس پروفایل گروه حذف شد !\nتوسط {}".format(
+                "**ɢʀᴏᴜᴘs  ᴘʀᴏғɪʟᴇ ᴘʜᴏᴛᴏ ʀᴇᴍᴏᴠᴇᴅ  !\nʙʏ** {}".format(
                     message.from_user.mention
                 )
             )
     except BaseException:
         await msg.edit(
-            "برای حذف عکس پروفایل گروه، کاربر باید دسترسی ادمین تغییر اطلاعات گروه را داشته باشد!"  # User must have admin rights to change group info
+            "**ᴛʜᴇ ᴜsᴇʀ ᴍᴏsᴛ ɴᴇᴇᴅ ᴄʜᴀɴɢᴇ ɪɴғᴏ ᴀᴅᴍɪɴ ʀɪɢʜᴛs ᴛᴏ ʀᴇᴍᴏᴠᴇ ɢʀᴏᴜᴘ ᴘʜᴏᴛᴏ !**"
         )
 
 
@@ -33,27 +34,28 @@ async def setchatphoto(_, message):
     reply = message.reply_to_message
     chat_id = message.chat.id
     user_id = message.from_user.id
-    msg = await message.reply_text("در حال پردازش...")
+    msg = await message.reply_text("ᴘʀᴏᴄᴇssɪɴɢ...")
     admin_check = await app.get_chat_member(chat_id, user_id)
     if message.chat.type == enums.ChatType.PRIVATE:
-        await msg.edit("این دستور فقط در گروه‌ها قابل استفاده است!")  # This command works only in groups
+        await msg.edit("`ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ ᴡᴏʀᴋ ᴏɴ ɢʀᴏᴜᴘs !`")
     elif not reply:
-        await msg.edit("لطفاً به یک عکس یا سند پاسخ دهید.")  # Please reply to a photo or document
+        await msg.edit("**ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴘʜᴏᴛᴏ ᴏʀ ᴅᴏᴄᴜᴍᴇɴᴛ.**")
     elif reply:
         try:
             if admin_check.privileges.can_change_info:
                 photo = await reply.download()
                 await message.chat.set_photo(photo=photo)
                 await msg.edit_text(
-                    "عکس پروفایل گروه تغییر یافت !\nتوسط {}".format(
+                    "**ɴᴇᴡ ɢʀᴏᴜᴘ ᴘʀᴏғɪʟᴇ ᴘʜᴏᴛᴏ ᴄʜᴀɴɢᴇᴅ !\nʙʏ** {}".format(
                         message.from_user.mention
                     )
                 )
             else:
-                await msg.edit("مشکلی پیش آمده، لطفاً عکس دیگری امتحان کنید!")  # Something went wrong, try another photo
+                await msg.edit("**sᴏᴍᴇᴛʜɪɴɢ ᴡʀᴏɴɢ ʜᴀᴘᴘᴇɴᴇᴅ ᴛʀʏ ᴀɴᴏᴛʜᴇʀ ᴘʜᴏᴛᴏ !**")
+
         except BaseException:
             await msg.edit(
-                "برای تغییر عکس پروفایل گروه، کاربر باید دسترسی ادمین تغییر اطلاعات گروه را داشته باشد!"  # User must have admin rights to change group photo
+                "**ᴛʜᴇ ᴜsᴇʀ ᴍᴏsᴛ ɴᴇᴇᴅ ᴄʜᴀɴɢᴇ ɪɴғᴏ ᴀᴅᴍɪɴ ʀɪɢʜᴛs ᴛᴏ ᴄʜᴀɴɢᴇ ɢʀᴏᴜᴘ ᴘʜᴏᴛᴏ !**"
             )
 
 
@@ -63,9 +65,9 @@ async def setgrouptitle(_, message):
     reply = message.reply_to_message
     chat_id = message.chat.id
     user_id = message.from_user.id
-    msg = await message.reply_text("در حال پردازش...")
+    msg = await message.reply_text("ᴘʀᴏᴄᴇssɪɴɢ...")
     if message.chat.type == enums.ChatType.PRIVATE:
-        await msg.edit("این دستور فقط در گروه‌ها قابل استفاده است!")  # This command works only in groups
+        await msg.edit("**ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ ᴡᴏʀᴋ ᴏɴ ɢʀᴏᴜᴘs !**")
     elif reply:
         try:
             title = message.reply_to_message.text
@@ -73,13 +75,13 @@ async def setgrouptitle(_, message):
             if admin_check.privileges.can_change_info:
                 await message.chat.set_title(title)
                 await msg.edit(
-                    "عنوان جدید گروه تنظیم شد !\nتوسط {}".format(
+                    "**ɴᴇᴡ ɢʀᴏᴜᴘ ɴᴀᴍᴇ ᴄʜᴀɴɢᴇᴅ !\nʙʏ** {}".format(
                         message.from_user.mention
                     )
                 )
         except AttributeError:
             await msg.edit(
-                "برای تغییر عنوان گروه، کاربر باید دسترسی ادمین تغییر اطلاعات گروه را داشته باشد!"  # User must have admin rights to change group title
+                "ᴛʜᴇ ᴜsᴇʀ ᴍᴏsᴛ ɴᴇᴇᴅ **ᴄʜᴀɴɢᴇ ɪɴғᴏ** ᴀᴅᴍɪɴ ʀɪɢʜᴛs ᴛᴏ ᴄʜᴀɴɢᴇ ɢʀᴏᴜᴘ ᴛɪᴛʟᴇ !"
             )
     elif len(message.command) > 1:
         try:
@@ -88,19 +90,18 @@ async def setgrouptitle(_, message):
             if admin_check.privileges.can_change_info:
                 await message.chat.set_title(title)
                 await msg.edit(
-
-            "عنوان جدید گروه تنظیم شد !\nتوسط {}".format(
+                    "**ɴᴇᴡ ɢʀᴏᴜᴘ ɴᴀᴍᴇ ᴄʜᴀɴɢᴇᴅ !\nʙʏ** {}".format(
                         message.from_user.mention
                     )
                 )
         except AttributeError:
             await msg.edit(
-                "برای تغییر عنوان گروه، کاربر باید دسترسی ادمین تغییر اطلاعات گروه را داشته باشد!"  # User must have admin rights to change group title
+                "**ᴛʜᴇ ᴜsᴇʀ ᴍᴏsᴛ ɴᴇᴇᴅ ᴄʜᴀɴɢᴇ ɪɴғᴏ ᴀᴅᴍɪɴ ʀɪɢʜᴛs ᴛᴏ ᴄʜᴀɴɢᴇ ɢʀᴏᴜᴘ ᴛɪᴛʟᴇ !**"
             )
 
     else:
         await msg.edit(
-            "لطفاً به یک متن پاسخ دهید یا عنوان جدیدی برای گروه وارد کنید."  # Please reply with a text or provide a new title for the group
+            "**ʏᴏᴜ ɴᴇᴇᴅ ʀᴇᴘʟʏ ᴛᴏ ᴛᴇxᴛ ᴏʀ ɢɪᴠᴇ sᴏᴍᴇ ᴛᴇxᴛ ᴛᴏ ᴄʜᴀɴɢᴇ ɢʀᴏᴜᴘ ᴛɪᴛʟᴇ **"
         )
 
 
@@ -110,9 +111,9 @@ async def setg_discription(_, message):
     reply = message.reply_to_message
     chat_id = message.chat.id
     user_id = message.from_user.id
-    msg = await message.reply_text("در حال پردازش...")
+    msg = await message.reply_text("**ᴘʀᴏᴄᴇssɪɴɢ...**")
     if message.chat.type == enums.ChatType.PRIVATE:
-        await msg.edit("این دستور فقط در گروه‌ها قابل استفاده است!")  # This command works only in groups
+        await msg.edit("**ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ ᴡᴏʀᴋs ᴏɴ ɢʀᴏᴜᴘs!**")
     elif reply:
         try:
             discription = message.reply_to_message.text
@@ -120,13 +121,13 @@ async def setg_discription(_, message):
             if admin_check.privileges.can_change_info:
                 await message.chat.set_description(discription)
                 await msg.edit(
-                    "توضیحات جدید گروه تغییر یافت !\nتوسط {}".format(
+                    "**ɴᴇᴡ ᴅɪsᴄʀɪᴘᴛɪᴏɴ ᴏғ ɢʀᴏᴜᴘ ᴄʜᴀɴɢᴇᴅ!**\nʙʏ {}".format(
                         message.from_user.mention
                     )
                 )
         except AttributeError:
             await msg.edit(
-                "برای تغییر توضیحات گروه، کاربر باید دسترسی ادمین تغییر اطلاعات گروه را داشته باشد!"  # User must have admin rights to change group description
+                "**ᴛʜᴇ ᴜsᴇʀ ᴍᴜsᴛ ʜᴀᴠᴇ ᴄʜᴀɴɢᴇ ɪɴғᴏ ᴀᴅᴍɪɴ ʀɪɢʜᴛs ᴛᴏ ᴄʜᴀɴɢᴇ ɢʀᴏᴜᴘ ᴅɪsᴄʀɪᴘᴛɪᴏɴ!**"
             )
     elif len(message.command) > 1:
         try:
@@ -135,15 +136,15 @@ async def setg_discription(_, message):
             if admin_check.privileges.can_change_info:
                 await message.chat.set_description(discription)
                 await msg.edit(
-                    "توضیحات جدید گروه تغییر یافت !\nتوسط {}".format(
+                    "**ɴᴇᴡ ᴅɪsᴄʀɪᴘᴛɪᴏɴ ᴏғ ɢʀᴏᴜᴘ ᴄʜᴀɴɢᴇᴅ!**\nʙʏ {}".format(
                         message.from_user.mention
                     )
                 )
         except AttributeError:
             await msg.edit(
-                "برای تغییر توضیحات گروه، کاربر باید دسترسی ادمین تغییر اطلاعات گروه را داشته باشد!"  # User must have admin rights to change group description
+                "**ᴛʜᴇ ᴜsᴇʀ ᴍᴜsᴛ ʜᴀᴠᴇ ᴄʜᴀɴɢᴇ ɪɴғᴏ ᴀᴅᴍɪɴ ʀɪɢʜᴛs ᴛᴏ ᴄʜᴀɴɢᴇ ɢʀᴏᴜᴘ ᴅɪsᴄʀɪᴘᴛɪᴏɴ!**"
             )
     else:
         await msg.edit(
-            "لطفاً به یک متن پاسخ دهید یا توضیحات جدیدی برای گروه وارد کنید."  # Please reply with a text or provide new description for the group
+            "**ʏᴏᴜ ɴᴇᴇᴅ ᴛᴏ ʀᴇᴘʟʏ ᴛᴏ ᴛᴇxᴛ ᴏʀ ɢɪᴠᴇ sᴏᴍᴇ ᴛᴇxᴛ ᴛᴏ ᴄʜᴀɴɢᴇ ɢʀᴏᴜᴘ ᴅɪsᴄʀɪᴘᴛᴏɴ!**"
         )
